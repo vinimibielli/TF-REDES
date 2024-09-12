4cac24f8e06cbb86e9811299e0b26857cb94eeaa
Lucas Damo <lucas.damo@edu.pucrs.br>
2024-09-12 20:38:36 -0300

    sockets and address for client and server initialization

--- srcs/ServerUCP.cpp
+++ srcs/ServerUCP.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
+}
