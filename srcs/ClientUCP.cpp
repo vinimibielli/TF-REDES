5f7e6c21821f0402ec8a80ba42e8ec531ab2d7ff
vinimibielli <vinimibielli@gmail.com>
2024-09-15 05:01:08 -0300

    Functional UDP client and server

--- srcs/ClientUCP.cpp
+++ srcs/ClientUCP.cpp
@@ -1,1 +1,81 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define MESSAGE_SERVER "Hello from client."
+
+void errorFunction(const std::string &message)
+{
+    std::cerr << message << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+int main()
+{
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddr;
+    socklen_t addrLen = sizeof(serverAddr);
+    std::string userIdentification = " ";
+    std::string messageUser;
+
+    // socket()
+
+    sockfd = (socket(AF_INET, SOCK_DGRAM, 0));
+    if (sockfd < 0)
+    {
+        errorFunction("Error to create the socket");
+    }
+
+    // set serverAddr
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_port = htons(PORT);
+
+    // recvfrom() and sendto()
+
+    while (true)
+    {
+
+        if (userIdentification == " ")
+        {
+            std::cout << "Enter your username: ";
+            std::getline(std::cin, messageUser);
+            userIdentification = messageUser;   
+        }
+        else
+        {
+            std::cout << "Enter Message to server: ";
+            std::getline(std::cin, messageUser);
+        }
+
+        int sendLen = sendto(sockfd, messageUser.c_str(), messageUser.length(), 0, (struct sockaddr *)&serverAddr, addrLen);
+        if (sendLen < 0)
+        {
+            std::cerr << "Error sending message" << std::endl;
+        }
+
+        std::cout << "Message sent to server" << std::endl;
+
+        int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&serverAddr, &addrLen);
+        if (recvLen < 0)
+        {
+            std::cerr << "Error to receive the message" << std::endl;
+        }
+
+        buffer[recvLen] = '\0';
+        std::cout << "Received response from server: " << buffer << std::endl;
+
+        // close()
+    }
+
+    close(sockfd);
+}
--- srcs/ServerUCP.cpp
+++ srcs/ServerUCP.cpp
@@ -8,11 +8,71 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define MESSAGE_SERVER "Hello from server."
 
-int main(){
+void errorFunction(const std::string &message){
+    std::cerr << message << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+int main()
+{
     int sockfd;
     char buffer[BUFFER_SIZE];
     struct sockaddr_in serverAddr;
     struct sockaddr_in clientAddr;
     socklen_t addrLen = sizeof(clientAddr);
+
+    // socket() -- create socket
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        errorFunction("Error to create the socket");
+    }
+
+    // set serverAddr -- setting up the servers address structures
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    // bind() -- binding the socket to the server address
+
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        close(sockfd);
+        errorFunction("Error to bind the socket");
+    }
+
+    // recvfrom() and sendto() -- functions to receive and send messages
+
+    while (true)
+    {
+        // recvfrom() -- receive messages
+        int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddr, &addrLen);
+        if (recvLen < 0)
+        {
+            std::cerr << "Error receiving data" << std::endl;
+            continue;
+        }
+
+        buffer[recvLen] = '\0'; // Null-terminate the received data
+        std::cout << "Received from client: " << buffer << std::endl;
+
+        // sendto() -- send messages
+        std::string messageUser;
+        std::cout << "Enter Message to server: ";
+        std::getline(std::cin, messageUser);
+        int sendLen = sendto(sockfd, messageUser.c_str(), messageUser.length(), 0, (struct sockaddr *)&clientAddr, addrLen);
+        if (sendLen < 0)
+        {
+            std::cerr << "Error sending data" << std::endl;
+            continue;
+        }
+    }
+
+    // close()
+
+    close(sockfd);
 }
