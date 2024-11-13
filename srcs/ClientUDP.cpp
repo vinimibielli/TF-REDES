aad19d68c53271e4ca48f1e59454eb1900b19069
vinimibielli <vinimibielli@gmail.com>
2024-11-13 11:44:20 -0300

    i think this will work

--- srcs/ClientUDP.cpp
+++ srcs/ClientUDP.cpp
@@ -17,10 +17,8 @@
 #include <sys/ioctl.h>
 
 #define PORT 9000
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 8192
 #define MESSAGE_SERVER "Hello from client."
-#define ROUTER_LIST_PREFIX "ROUTER_LIST:"
-#define MSG_PREFIX "MSG:"
 
 std::vector<std::pair<std::string, std::pair<std::string, int>>> ipList;
 std::vector<std::pair<std::string, int>> vizinhos;
@@ -32,11 +30,38 @@ void errorFunction(const std::string &message)
     exit(EXIT_FAILURE);
 }
 
+void sendMessage(int sockfd, std::string router, std::string &message)
+{
+    struct sockaddr_in routerAddr;
+    routerAddr.sin_family = AF_INET;
+    routerAddr.sin_port = htons(PORT);
+    routerAddr.sin_addr.s_addr = INADDR_ANY;
+    socklen_t addrLen = sizeof(routerAddr);
+
+    if(ipList.size() > 0) {
+        for(int i = 0; i < ipList.size(); i++) {
+            if(ipList[i].second.first == router) {
+                routerAddr.sin_addr.s_addr = inet_addr(ipList[i].first.c_str());
+                break;
+            }
+        }
+    }
+
+    int sendLen = sendto(sockfd, message.c_str(), message.length(), 0, (struct sockaddr *)&routerAddr, addrLen);
+    if (sendLen < 0){
+        std::cerr << "Error sending message" << std::endl;
+    }
+}
+
 void receiveMessage(int sockfd)
 {
     char buffer[BUFFER_SIZE];
     struct sockaddr_in receiveAddr;
+    receiveAddr.sin_family = AF_INET;
+    receiveAddr.sin_port = htons(PORT);
+    receiveAddr.sin_addr.s_addr = INADDR_ANY;
     socklen_t addrLen = sizeof(receiveAddr);
+
     while (true)
     {
         int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&receiveAddr, &addrLen);
@@ -56,23 +81,36 @@ void receiveMessage(int sockfd)
             buffer[recvLen] = '\0';
 
             std::string message(buffer);
-            if (message.rfind(ROUTER_LIST_PREFIX, 0) == 0)
-            {
-                std::string routerList = message.substr(strlen(ROUTER_LIST_PREFIX));
-                std::string delimiter = ";";
+            std::cout << "MENSAGEM RECEBIDA: " << message << "\n";
+            if (message[0] == '@')
+            {   
+                std::cout << "RECEBEMOS UMA TABELA PORRAA!!!!!!!!" << std::endl;
+                // continue;
+                std::string routerList = message.substr(1); // remove 1st char
+                std::string delimiter = "@";
                 std::string ipReceive = std::string(inet_ntoa(receiveAddr.sin_addr));
-
                 while (routerList.size() > 0)
                 {
                     // separando o ip e a métrica
                     int posIP = routerList.find(delimiter);
+                    if (posIP == -1) {
+                        //chegou no ultimo IP
+                        posIP = routerList.length();
+                    }
+                    
+                    // <ip>-<metrica>
                     std::string router = routerList.substr(0, posIP);
 
                     // separando a métrica para fazer o incremento
                     int posMetric = router.find("-");
+
+                    // ip e metrica separados
                     std::string ip = router.substr(0, posMetric);
-                    std::string metricString = router.substr(posMetric + 1);
-                    int metric = std::stoi(metricString);
+                    int metric = std::stoi(router.substr(posMetric + 1));
+
+                    // teste printando eles
+                    std::cout << "router:" << router << " -> " << ip << "-" << metric << "\n";
+
 
                     // verificando se o ip já está na lista e caso não esteja, adicionando na lista e com a métrica maior
                     bool found = false;
@@ -89,6 +127,7 @@ void receiveMessage(int sockfd)
                             if (ipList[i].second.first == ip)
                             {
                                 found = true;
+                                std::cout << "encontrou igual ou o localIP" << std::endl;
                                 if (metric < ipList[i].second.second)
                                 {
                                     ipList[i].second.first = metric;
@@ -99,18 +138,50 @@ void receiveMessage(int sockfd)
                         }
                     }
 
-                    if (!found)
-                    {
-
-                        routerList.erase(0, posIP + delimiter.length());
-                        std::cout << "New router added: " << ip << std::endl;
+                    if (!found) {
+                        if(ip != localIp) {
+                            // JA TA FAZENDO ESSE COMANDO NO FINAL, NAO PRECISA FAZER AQUI
+                            // routerList.erase(0, posIP + delimiter.length());
+                            ipList.push_back(std::make_pair(ipReceive, std::make_pair(ip, (metric + 1))));
+                            std::cout << "New router added: " << ip << std::endl;
+                            // found = false; // precisa declarar isso como false? o bgl ja n vai ser mais usado msm
+                        }
                     }
+
+                    // apagando a parte usada da mensagem para ir para a proxima 
+                    routerList.erase(0, posIP + delimiter.length());
+                    continue; 
                 }
             }
-            else if (message.rfind(MSG_PREFIX, 0) == 0)
+            else if (message[0] == '!')
             {
-                std::string userMessage = message.substr(strlen(MSG_PREFIX));
-                std::cout << userMessage << std::endl;
+                std::string messageDelimiter = ";";
+                std::string userMessage = message;
+                if(userMessage.find(localIp) == std::string::npos) {
+                    std::cout << "Repassei a mensagem: " << userMessage << std::endl;
+                    std::string routerOrigem = userMessage.substr(0, userMessage.find(messageDelimiter));
+                    std::string routerDestino = userMessage.substr(0, userMessage.find(messageDelimiter));
+                    sendMessage(sockfd, routerDestino, userMessage);
+                } else {
+                    std::cout << "Mensagem recebida: " << userMessage << std::endl;
+                }
+            }
+            else if (message[0] == '*') {
+                // novo roteador se conectou, vizinho novo, tem q adicionar na lista com metrica 0, olha no enunciado do trabalho
+                bool exists = false;
+                std::string ipVizinho = message.substr(1);
+                std::cout << "New router connected: " << ipVizinho << std::endl;
+                for(int i = 0; i < ipList.size(); i++) {
+                    if(ipList[i].second.first == ipVizinho) {
+                        exists = true;
+                        break;
+                    }
+                }
+                if(!exists) {
+                    ipList.push_back(std::make_pair(ipVizinho, std::make_pair(ipVizinho, 1)));
+                    vizinhos.push_back(std::make_pair(ipVizinho, 35));
+                    exists = false;
+                }
             }
         }
 
@@ -118,39 +189,19 @@ void receiveMessage(int sockfd)
     }
 }
 
-// void sendMessage(int sockfd, struct sockaddr_in &routerAddr, socklen_t &addrLen, std::string &message)
-// {
-//     while (true)
-//     {
-//         getline(std::cin, message);
-//         for (int i = 0; i < ipList.size(); i++)
-//         {
-//             routerAddr.sin_addr.s_addr = inet_addr(ipList[i].first.c_str());
-//             int sendLen = sendto(sockfd, message.c_str(), message.length(), 0, (struct sockaddr *)&routerAddr, addrLen);
-//             if (sendLen < 0)
-//             {
-//                 std::cerr << "Error sending message" << std::endl;
-//             }
-//         }
-//         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//     }
-// }
-
 void sendIpList(int sockfd)
 {
-
     struct sockaddr_in routerAddr;
     socklen_t addrLen = sizeof(routerAddr);
-    std::string message = ROUTER_LIST_PREFIX;
     routerAddr.sin_family = AF_INET;
     routerAddr.sin_port = htons(PORT);
 
     while (true)
     {
-
+        std::string message = "";
         for (int i = 0; i < ipList.size(); i++)
         {
-            message += ipList[i].second.first + "-" + std::to_string(ipList[i].second.second) + ";";
+            message += "@" + ipList[i].second.first + "-" + std::to_string(ipList[i].second.second);
         }
 
         if (ipList.size() > 0)
@@ -158,6 +209,7 @@ void sendIpList(int sockfd)
             for (int i = 0; i < ipList.size(); i++)
             {
                 routerAddr.sin_addr.s_addr = inet_addr(ipList[i].first.c_str());
+                std::cout << "IpList foi enviada para o ip: " << ipList[i].first << std::endl;
                 int sendLen = sendto(sockfd, message.c_str(), message.length(), 0, (struct sockaddr *)&routerAddr, addrLen);
                 if (sendLen < 0)
                 {
@@ -188,62 +240,58 @@ void printIpList()
     }
 }
 
-void countDisconnect()
-{
-    while (true)
-    {
-        for (int i = 0; i < vizinhos.size(); i++)
-        {
-            if (vizinhos[i].second == 0)
-            {
+void countDisconnect() {
+    while (true) {
+        for (int i = 0; i < vizinhos.size(); i++) {
+            if (vizinhos[i].second == 0) {
                 ipList.erase(ipList.begin() + i);
                 vizinhos.erase(vizinhos.begin() + i);
                 std::cout << "Router " << vizinhos[i].first << " disconnected" << std::endl;
             }
-
-            if (vizinhos[i].second > 0)
-            {
+            else if (vizinhos[i].second > 0)
                 vizinhos[i].second--;
-            }
         }
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
     int sockfd;
     char buffer[BUFFER_SIZE];
     struct sockaddr_in routerAddr;
     socklen_t addrLen = sizeof(routerAddr);
     std::string messageUser;
     localIp = argv[1];
-    std::cout << "IP Address of user: " << localIp << std::endl;
+    // std::cout << "IP Address of user: " << localIp << std::endl;
 
     // socket();
 
     sockfd = (socket(AF_INET, SOCK_DGRAM, 0));
     if (sockfd < 0)
-    {
         errorFunction("Error to create the socket");
-    }
-
+    
     // routerAddr();
     routerAddr.sin_family = AF_INET;
     routerAddr.sin_port = htons(PORT);
     routerAddr.sin_addr.s_addr = INADDR_ANY;
 
+    if (bind(sockfd, (struct sockaddr *)&routerAddr, sizeof(routerAddr)) < 0) {
+        close(sockfd);
+        errorFunction("Error to bind the socket");
+    }
+
     std::ifstream file("roteadores.txt");
 
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
         std::cerr << "Error opening file: roteadores.txt" << std::endl;
+        return -1;
+    }
+    else {
+        std::cout << "Arquivo de configuracao lido";
     }
 
     std::string line;
-    while (std::getline(file, line))
-    {
+    while (std::getline(file, line)) {
         std::cout << "Adding router: " << line << std::endl;
         ipList.push_back(std::make_pair(line, std::make_pair(line, 1)));
         vizinhos.push_back(std::make_pair(line, 35));
@@ -265,24 +313,25 @@ int main(int argc, char *argv[])
     receiveThread.detach();
 
     // Periodically send IP list
-
     std::thread sendIpListThread(sendIpList, sockfd);
     sendIpListThread.detach();
 
     while (true)
     {
+        std::cout << "Iplist size: " << ipList.size()<< std::endl;
         // std::cout << "Digite a mensagem: ";
+        std::string messageSend = "!";
         getline(std::cin, messageUser);
-        std::cout << "IpList size: " << ipList.size() << std::endl;
+        messageSend += ' ' + messageUser;
+        std::cout << "Message to the other user: " << messageSend << std::endl;
         for (int i = 0; i < ipList.size(); i++)
         {
             std::cout << "Enviando para: " << ipList[i].first << std::endl;
             routerAddr.sin_addr.s_addr = inet_addr(ipList[i].first.c_str());
             int sendLen = sendto(sockfd, messageUser.c_str(), messageUser.length(), 0, (struct sockaddr *)&routerAddr, addrLen);
             if (sendLen < 0)
-            {
                 std::cerr << "Error sending message" << std::endl;
-            }
+            
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
